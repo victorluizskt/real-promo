61ba726504c7ec9f9cc8ee8c070ddf2793494f83
Victor Luiz <c0007043@vli-logistica.com.br>
2022-12-26 19:36:39 -0300

    add views

--- RealPromo.API/Controllers/HomeController.cs
+++ RealPromo.API/Controllers/HomeController.cs
@@ -4,6 +4,19 @@ namespace RealPromo.API.Controllers
 {
     public class HomeController : Controller
     {
-        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Promocao()
+        {
+            return View();
+        }
+
+        public ActionResult CadastrarPromocao()
+        {
+            return View();
+        }
     }
 }
